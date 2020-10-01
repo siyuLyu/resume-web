cf1fb0f03f1c94057b1a00f9bd5e4806542d8017
siyulyu <lyu.si@husky.neu.edu>
2020-09-30 22:49:24 -0700

    Updates

---
+++ precache-manifest.b8626d4fe900d489622e89302d903482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe700ffb70ee3c4af7c7b270689330b",
+    "url": "/resume-web/index.html"
+  },
+  {
+    "revision": "0fd7c0db646031f092dc",
+    "url": "/resume-web/static/css/main.6315b0a8.chunk.css"
+  },
+  {
+    "revision": "c3d1817bedbb6ee1100f",
+    "url": "/resume-web/static/js/2.d0c85195.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/resume-web/static/js/2.d0c85195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd7c0db646031f092dc",
+    "url": "/resume-web/static/js/main.368d0f1b.chunk.js"
+  },
+  {
+    "revision": "b524f2e4d4611d44f71f",
+    "url": "/resume-web/static/js/runtime-main.c071d494.js"
+  }
+]);
